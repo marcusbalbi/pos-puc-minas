03f223103af33f1988c504e8a85446f98ffc6c7f
Marcus Balbi <balbimarcus@gmail.com>
2021-05-20 17:22:45 -0300

    working

--- 03_arquitetura_de_backend/service_discovery/microservice-a/index.js
+++ 03_arquitetura_de_backend/service_discovery/microservice-a/index.js
@@ -1,4 +1,33 @@
 const express = require("express");
+const Eureka = require("eureka-js-client").Eureka;
+
+
+// example configuration
+const client = new Eureka({
+  instance: {
+    app: 'microservice1',
+    hostName: 'localhost',
+    ipAddr: '127.0.0.1',
+    statusPageUrl: 'http://localhost:3000/',
+    port: {
+      '$': 3000,
+      '@enabled': 'true',
+    },
+    vipAddress: 'http://localhost:3000/',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      name: 'MyOwn',
+    },
+  },
+  eureka: {
+    host: "eureka",
+    port: 8761,
+    servicePath: "/eureka/apps/",
+  },
+});
+
+
+client.start();
 
 const app = express();
 
@@ -8,4 +37,5 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
   console.log(`Server started on 3000`);
+
 });
