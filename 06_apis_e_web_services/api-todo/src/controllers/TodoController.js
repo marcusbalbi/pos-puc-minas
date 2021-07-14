bf1f107ecd050c8655671c4fd4c24ac952bb4f4e
Marcus Balbi <balbimarcus@gmail.com>
2021-07-13 22:08:08 -0300

    add, get and getAll

--- 06_apis_e_web_services/api-todo/src/controllers/TodoController.js
+++ 06_apis_e_web_services/api-todo/src/controllers/TodoController.js
@@ -1,12 +1,39 @@
 const TodoService = require("../services/TodoService");
 
 const TodoController = {
-  get: (req, res) => {},
-  getAll: async (req, res) => {
-    const todos = await TodoService.getAllTodos();
-    res.json(todos);
+  get: async (req, res) => {
+    try {
+      const todo = await TodoService.get(req.params.id);
+      if (!todo) {
+        return res
+          .status(404)
+          .json({ message: "There is no Todo with this id!" });
+      }
+      res.status(200).json(todo);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   },
-  add: (req, res) => {},
+  getAll: async (req, res) => {
+    try {
+      const todos = await TodoService.getAllTodos();
+      if (!todos || todos.length == 0) {
+        return res.status(404).json({ message: "There is no Todo yet!" });
+      }
+      res.status(200).json(todos);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+  add: async (req, res) => {
+    try {
+      const { title, description } = req.body;
+      const id = await TodoService.add(title, description);
+      res.status(201).json({ message: `Todo Created ID: ${id} ` });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
   update: (req, res) => {},
   delete: (req, res) => {},
 };
--- 06_apis_e_web_services/api-todo/src/services/TodoService.js
+++ 06_apis_e_web_services/api-todo/src/services/TodoService.js
@@ -1,12 +1,33 @@
 const Todo = require("../models/Todo");
 
 const TodoService = {
-  async getAllTodos () {
-    return Todo.find({});
+  async get(id) {
+    const todo = await Todo.findById(id);
+    return {
+      id: todo._id,
+      title: todo.title,
+      description: todo.description,
+      date: todo.date,
+      finished: todo.finished,
+    };
   },
-  async add (title, description) {
-    Todo.create({title, description, finished: false})
-  }
-}
+  async getAllTodos() {
+    const todos = await Todo.find({});
+    return todos.map((todo) => {
+      return {
+        id: todo._id,
+        title: todo.title,
+        description: todo.description,
+        date: todo.date,
+        finished: todo.finished,
+      };
+    });
+  },
+  async add(title, description) {
+    const todo = await Todo.create({ title, description, finished: false });
+
+    return todo._id;
+  },
+};
 
 module.exports = TodoService;
